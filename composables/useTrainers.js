bd2bc86db407d0ae1a35879b6c56f905acddc111
Yuji Nitta <32283524+11258@users.noreply.github.com>
2022-08-24 14:33:47 +0900

    update code2

--- composables/useTrainers.js
+++ composables/useTrainers.js
@@ -3,6 +3,6 @@ import { VITE_SERVER_ORIGIN } from "~/utils/env";
 
 export default async () => {
   console.log(`VITE_SERVER_ORIGIN - ${VITE_SERVER_ORIGIN}`)
-  const response = useFetch(`${VITE_SERVER_ORIGIN}/api/trainers`);
+  const response = useFetch(`${VITE_SERVER_ORIGIN}api/trainers`);
   return response;
 };
